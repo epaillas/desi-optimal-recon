1a3d6994a8044325cd445033794d3b608008f489
epaillas <enrique.paillas@waterloo.ca>
2023-05-22 18:59:01 -0700

    init

---
+++ desi_tracers_multipoles.py
@@ -0,0 +1,98 @@
+import numpy as np
+from pathlib import Path
+from pypower import CatalogFFTPower
+import matplotlib.pyplot as plt
+plt.style.use(['enrique-science'])
+
+# BGS
+multipoles_phases = []
+for phase in range(25):
+    data_dir = Path('/global/cfs/cdirs/desi/cosmosim/KP45/MC/Clustering/AbacusSummit/CutSky/BGS/Pk/Pre/epaillas')
+    data_fn = data_dir / f'BGS_PreRecon_Pk_Mr-21.5_mr19.5_zmin0.1_zmax0.4_ph{phase:03}.npy'
+    result = CatalogFFTPower.load(data_fn)
+    poles = result.poles[::5]
+    poles.select((0, 0.3))
+    k_bgs = poles.k
+    Pk_0 = poles(ell=0, complex=False)
+    Pk_2 = poles(ell=2, complex=False)
+    Pk_4 = poles(ell=4, complex=False)
+    multipoles_phases.append([Pk_0, Pk_2, Pk_4])
+multipoles_phases = np.asarray(multipoles_phases)
+multipoles_bgs = multipoles_phases.mean(axis=0)
+err_bgs = multipoles_phases.std(axis=0)
+
+# LRG
+multipoles_phases = []
+for phase in range(25):
+    data_dir = Path('/global/cfs/cdirs/desi/cosmosim/KP45/MC/Clustering/AbacusSummit/CutSky/LRG/Pk/Pre/zhejie/NGC')
+    data_fn = data_dir / f'cutsky_LRG_NGC_0.8z1.1_ph{phase:03}.randoms20X.pre_recon_redshiftspace_pad1.5_Pk_nmesh1024.npy'
+    result = CatalogFFTPower.load(data_fn)
+    poles = result.poles
+    poles.select((0, 0.3))
+    k_lrg = poles.k
+    Pk_0 = poles(ell=0, complex=False)
+    Pk_2 = poles(ell=2, complex=False)
+    Pk_4 = poles(ell=4, complex=False)
+    multipoles_phases.append([Pk_0, Pk_2, Pk_4])
+multipoles_phases = np.asarray(multipoles_phases)
+multipoles_lrg = multipoles_phases.mean(axis=0)
+err_lrg = multipoles_phases.std(axis=0)
+
+# QSO
+multipoles_phases = []
+for phase in range(25):
+    data_dir = Path('/global/cfs/cdirs/desi/cosmosim/KP45/MC/Clustering/AbacusSummit/CutSky/QSO/Pk/Pre/epaillas/')
+    data_fn = data_dir / f'QSO_PreRecon_Pk_zmin0.8_zmax1.6_ph{phase:03}.npy'
+    result = CatalogFFTPower.load(data_fn)
+    poles = result.poles[::5]
+    poles.select((0, 0.3))
+    k_qso = poles.k
+    Pk_0 = poles(ell=0, complex=False)
+    Pk_2 = poles(ell=2, complex=False)
+    Pk_4 = poles(ell=4, complex=False)
+    multipoles_phases.append([Pk_0, Pk_2, Pk_4])
+multipoles_phases = np.asarray(multipoles_phases)
+multipoles_qso = multipoles_phases.mean(axis=0)
+err_qso = multipoles_phases.std(axis=0)
+
+fig, ax = plt.subplots(1, 4, figsize=(10, 3), sharex=True, sharey=True, layout='constrained')
+phases = list(range(0, 25))
+
+ax[0].plot(k_bgs, k_bgs*multipoles_bgs[0], label='$\ell = 0$')
+ax[0].plot(k_bgs, k_bgs*multipoles_bgs[1], label='$\ell = 2$')
+ax[0].plot(k_bgs, k_bgs*multipoles_bgs[2], label='$\ell = 4$')
+ax[0].fill_between(k_bgs, k_bgs*(multipoles_bgs[0] - err_bgs[0]), k_bgs*(multipoles_bgs[0] + err_bgs[0]), alpha=0.3)
+ax[0].fill_between(k_bgs, k_bgs*(multipoles_bgs[1] - err_bgs[1]), k_bgs*(multipoles_bgs[1] + err_bgs[1]), alpha=0.3)
+ax[0].fill_between(k_bgs, k_bgs*(multipoles_bgs[2] - err_bgs[2]), k_bgs*(multipoles_bgs[2] + err_bgs[2]), alpha=0.3)
+
+ax[1].plot(k_lrg, k_lrg*multipoles_lrg[0], label='$\ell = 0$')
+ax[1].plot(k_lrg, k_lrg*multipoles_lrg[1], label='$\ell = 2$')
+ax[1].plot(k_lrg, k_lrg*multipoles_lrg[2], label='$\ell = 4$')
+ax[1].fill_between(k_lrg, k_lrg*(multipoles_lrg[0] - err_lrg[0]), k_lrg*(multipoles_lrg[0] + err_lrg[0]), alpha=0.3)
+ax[1].fill_between(k_lrg, k_lrg*(multipoles_lrg[1] - err_lrg[1]), k_lrg*(multipoles_lrg[1] + err_lrg[1]), alpha=0.3)
+ax[1].fill_between(k_lrg, k_lrg*(multipoles_lrg[2] - err_lrg[2]), k_lrg*(multipoles_lrg[2] + err_lrg[2]), alpha=0.3)
+
+ax[3].plot(k_qso, k_qso*multipoles_qso[0], label='$\ell = 0$')
+ax[3].plot(k_qso, k_qso*multipoles_qso[1], label='$\ell = 2$')
+ax[3].plot(k_qso, k_qso*multipoles_qso[2], label='$\ell = 4$')
+ax[3].fill_between(k_qso, k_qso*(multipoles_qso[0] - err_qso[0]), k_qso*(multipoles_qso[0] + err_qso[0]), alpha=0.3)
+ax[3].fill_between(k_qso, k_qso*(multipoles_qso[1] - err_qso[1]), k_qso*(multipoles_qso[1] + err_qso[1]), alpha=0.3)
+ax[3].fill_between(k_qso, k_qso*(multipoles_qso[2] - err_qso[2]), k_qso*(multipoles_qso[2] + err_qso[2]), alpha=0.3)
+
+    
+ax[0].set_ylabel(r'$kP_{\ell}(k)\,[h^2{\rm Mpc^{-2}}]$', fontsize=15)
+leg = ax[0].legend(handlelength=0.0, labelspacing=0.1)
+for line,text in zip(leg.get_lines(), leg.get_texts()):
+    text.set_color(line.get_color())
+fig.supxlabel(r'$k\,[h{\rm Mpc^{-1}}]$', fontsize=15)
+for aa in ax:
+    aa.tick_params(axis='x', labelsize=15)
+    aa.tick_params(axis='y', labelsize=15)
+
+titles = ['BGS', 'LRG', 'ELG', 'QSO']
+for i, aa in enumerate(ax):
+    aa.set_title(titles[i], fontsize=15)
+# ax[0].set_xlim(0, 0.3)
+# plt.savefig('fig/ezmocks_multipoles.png', dpi=300)
+# plt.show()
+plt.savefig('fig/png/desi_tracers_multipoles.png', dpi=500)
---
+++ desi_tracers_nz.py
@@ -0,0 +1,63 @@
+import numpy as np
+from pathlib import Path
+import fitsio
+import matplotlib.pyplot as plt
+plt.style.use(['enrique-science'])
+
+def read_catalog(filename, zmin=0.8, zmax=3.5, cap='NGC'):
+    """Read a cutsky catalogue and return positions in comoving
+    cartesian coordinates."""
+    data = fitsio.read(filename)
+    print(data.dtype.names)
+    Y5_mask = data['STATUS'] & 2**1 != 0
+    redshift_mask = (data['Z'] > zmin) & (data['Z'] < zmax)
+    if cap == 'NGC':
+        cap_mask = (data['RA'] > 80) & (data['RA'] < 300)
+    else:
+        cap_mask = (data['RA'] < 80) | (data['RA'] > 300)
+    mask = Y5_mask & cap_mask & redshift_mask
+    ra = data['RA'][mask]
+    dec = data['DEC'][mask]
+    z = data['Z'][mask]
+    nz = data['NZ'][mask]
+    return ra, dec, z, nz
+
+
+samples = ['LRG', 'ELG', 'QSO']
+zrange_samples = [
+    [[0.4, 0.6], [0.6, 0.8], [0.8, 1.1]],
+    [[0.6, 0.8], [0.8, 1.1], [1.1, 1.6]],
+    [[0.8, 1.6], [1.6, 2.1], [2.1, 3.5]],
+
+]
+root_dir = '/global/homes/e/epaillas/analyse/kp45/bgs/FirstGenMocks/AbacusSummit/CutSky/'
+fname_samples = [
+    root_dir + 'LRG/z0.800/cutsky_LRG_z0.800_AbacusSummit_base_c000_ph000.fits',
+    root_dir + 'ELG/z1.100/cutsky_ELG_z1.100_AbacusSummit_base_c000_ph000.fits',
+    root_dir + 'QSO/z1.400/cutsky_QSO_z1.400_AbacusSummit_base_c000_ph000.fits',
+]
+
+
+fig, ax = plt.subplots(1, 4, figsize=(11, 3), sharey=True, layout='constrained')
+for i, sample in enumerate(samples):
+    
+    for zrange in zrange_samples[i]:
+        zmin, zmax = zrange
+        ra, dec, z, nz = read_catalog(fname_samples[i], zmin=zmin, zmax=zmax, cap='NGC')
+        nsub = 10_000
+        ax[i].scatter(z[::nsub], 1e4 * nz[::nsub], s=5.0, marker='.')
+for aa in ax:
+    # aa.set_xlabel('redshift ''$z$', fontsize=15)
+    aa.tick_params(axis='x', labelsize=15)
+    aa.tick_params(axis='y', labelsize=15)
+ax[0].set_ylabel(r'$n(z)\, [10^4 h^{3}{\rm Mpc^{-3}}]$', fontsize=15)
+ax[0].annotate(text='LRG', xy=(0.5, 15), fontsize=15) 
+ax[1].annotate(text='ELG', xy=(0.9, 2.5), fontsize=15)
+ax[2].annotate(text='QSO', xy=(2.0, 5), fontsize=15)
+ax[3].annotate(text='BGS', xy=(0.4, 15), fontsize=15)
+# fig.text(0.5, 0.04, 'redshift ''$z$', ha='center')
+fig.supxlabel('redshift ''$z$', fontsize=15)
+# plt.tight_layout()
+plt.subplots_adjust(wspace=0.05)
+plt.savefig('fig/png/desi_tracers_nz.png', dpi=500)
+# plt.show()
