6872ec7a39de5d7c1aefe75a3d3935ade7746c96
epaillas <enrique.paillas@waterloo.ca>
2023-10-14 19:36:38 -0700

    add SecondGen recon scripts

---
+++ scripts/SecondGenMocks_recon_LRG.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source /global/common/software/desi/desi_environment.sh main
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+export PATH=/global/homes/e/epaillas/code/LSS/bin:$PATH
+export PYTHONPATH=/global/homes/e/epaillas/code/LSS/py:$PYTHONPATH
+module swap pyrecon/main pyrecon/mpi
+# JOB_FLAGS="-n 128 -N 1 -C cpu -t 04:00:00 --qos interactive --account desi"
+JOB_FLAGS="-n 128"
+
+for PHASE in {15..15}
+do
+    TRACER="LRG_ffa"
+    REGION="NGC SGC"
+    ZLIM="0.4 1.1"
+    ALGORITHM="IFT"
+    CONVENTION="recsym"
+    SMOOTHING_RADIUS=10
+    GROWTH_RATE=0.834
+    BIAS=1.99
+    CELLSIZE=4.0
+    NRAN=18
+    INPUT_DIR=/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/mock$PHASE/
+    OUTPUT_DIR=/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/mock$PHASE/recon_sm"$SMOOTHING_RADIUS"
+    CODE_PATH=/global/homes/e/epaillas/code/LSS/scripts/recon.py
+
+    time srun $JOB_FLAGS python $CODE_PATH \
+        --tracer $TRACER \
+        --zlim $ZLIM \
+        --indir $INPUT_DIR \
+        --outdir $OUTPUT_DIR \
+        --algorithm $ALGORITHM \
+        --convention $CONVENTION \
+        --f $GROWTH_RATE \
+        --bias $BIAS \
+        --cellsize $CELLSIZE \
+        --smoothing_radius $SMOOTHING_RADIUS \
+        --region $REGION \
+        --nran $NRAN
+
+done
